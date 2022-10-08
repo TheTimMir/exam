a6b7c730c77532820ffffafefcdd599d94e56e82
Тим <mirocnicencotimofej2008@gmail.com>
2022-10-08 12:36:32 +0300

    Added testing function

--- main.py
+++ main.py
@@ -50,3 +50,25 @@ class Person:
         return f"Name:{self.name}\nHealth:{self.health}\nHappiness:{self.happiness}\nCapital:{self.capital}"
 
 
+def testing():
+    create_video = Action("Create video", -3, 5, 1000)
+    lift = Work("Lift boxes", 1, -5, 100)
+    workout = Rest("Do a workout", 10, 1, -150)
+
+    rich_man = Person("Travis", 34, 87, 1000000.0)
+    reg_man = Person("Igor", 70, 68, 100000.0)
+    poor_man = Person("Andrew", 32, 5, 100.0)
+
+    rich_man.do(create_video)  # does smth middle
+    print(f"{rich_man}\n")  # returns rich man's status
+
+    reg_man.do(workout)  # rests by doing a workout
+    print(f"{reg_man}\n")  # returns regular man's status
+
+    poor_man.do(lift)  # works by lifting boxes
+    print(f"{poor_man}\n")  # returns poor man's status
+
+    reg_man.change_status(0, 0, -1000)  # you can manually change stats for, per example, fines
+    print(f"{reg_man}\n")
+
+testing()
